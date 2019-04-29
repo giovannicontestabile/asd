4196242ba265b7e7ddfbcf7d491f97eedaf9a556
giovannicontestabile <46928006+giovannicontestabile@users.noreply.github.com>
2019-04-29 17:37:39 +0200

    Add files via upload

---
+++ mongolicani/src/main/java/it/giovanni/mongolicani/MongolicaniApplication.java
@@ -0,0 +1,13 @@
+package it.giovanni.mongolicani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongolicaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongolicaniApplication.class, args);
+	}
+
+}
